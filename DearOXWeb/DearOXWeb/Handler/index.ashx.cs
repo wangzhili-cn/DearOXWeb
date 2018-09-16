9340193b60314775d0af138cc7c9c7b4045b3284
wangzhili <wangzhili.cn@gmail.com>
2018-09-16 16:21:14 +0800

    jichujianshe
    jichujianshe

---
+++ DearOXWeb/DearOXWeb/Handler/index.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DearOXWeb.Handler
+{
+    /// <summary>
+    /// index 的摘要说明
+    /// </summary>
+    public class index : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
